d0777152caaa28e9860d7900c4ce4a948c4570e9
Anton Shevchenya <shevchenya.a@gmail.com>
2017-01-23 23:41:07 +0300

    initial commit

---
+++ voting-client/run_server.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+export NODE_ENV=development
+webpack-dev-server --config config/webpack.config.dev.js
---
+++ voting-client/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Voting from './components/Voting'
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <Voting pair={pair} winner="Trainspotting"/>,
+  document.getElementById('root')
+);
